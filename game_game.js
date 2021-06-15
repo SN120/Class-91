7aececc8f38c2e18c67e771c2d4326fd4adeddd8
SN120 <79451292+SN120@users.noreply.github.com>
2021-06-15 18:02:37 +0530

    Add files via upload

---
+++ game_game.js
@@ -0,0 +1,42 @@
+Player_1=localStorage.getItem("Player_1");
+Player_2=localStorage.getItem("Player_2");
+Player_1_score=0;
+Player_2_score=0;
+
+document.getElementById("player_1").innerHTML=Player_1+":";
+document.getElementById("player_2").innerHTML=Player_2+":";
+
+document.getElementById("player1_score").innerHTML=Player_1_score;
+document.getElementById("player2_score").innerHTML=Player_2_score;
+
+document.getElementById("Player_question").innerHTML="Question Turn - "+ Player_1;
+document.getElementById("Player_answer").innerHTML="Answer Turn - "+ Player_2;
+
+function Send(){
+    get_word = document.getElementById("word").value;
+    word = get_word.toLowerCase();
+    console.log("Word in Lowercase - "+word);
+
+    charAt1=word.charAt(1);
+    console.log(charAt1);
+
+    length_divide=Math.floor(word.length/2);
+    
+    charAt2=word.charAt(length_divide);
+    console.log(charAt2);
+
+    charAt3=word.charAt(word.length-1);
+    console.log(charAt3);
+
+    remove_charAt1 = word.replace(charAt1, "_");
+    remove_charAt2 = remove_charAt1.replace(charAt2, "_");
+    remove_charAt3 = remove_charAt2.replace(charAt3, "_");
+    console.log(remove_charAt3);
+
+    question_1Word = "<h4 id='word_display'>Q. " + remove_charAt3 + "</h4>";
+    input_box = "<br> Answer : <input type='text' id='input_check_box'>";
+    check_button="<br><br><button class='btn btn-info' onClick='check();'>Check</button>";
+    row=question_1Word+input_box+check_button;
+    document.getElementById("output").innerHTML=row;
+    document.getElementById("word").value="";
+}
